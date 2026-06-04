30e44a70455450c759744cf20731714c4420d4a3
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-04 16:31:20 +0530

    fix: update API endpoint to use the correct URL for dummyjson.com

--- api/quote.js
+++ api/quote.js
@@ -4,7 +4,7 @@ module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
-    const response = await fetch("dummyjson.com/quotes");
+    const response = await fetch("https://dummyjson.com");
     if (!response.ok) throw new Error("Quotable API responded with " + response.status);
 
     const data = await response.json();
